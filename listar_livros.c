01e76dd3579f33c29ee179c8d160398af3a0564a
Lucas Marques <lucas.smarques@outlook.com>
2015-11-21 19:42:48 -0200

    Implementada funcao para listar livros do arquivo

--- listar_livros.c
+++ listar_livros.c
@@ -1,6 +1,25 @@
-#include "listar_livros.h"
 #include <stdio.h>
+#include <string.h>
+#include "livro.h"
+#include "listar_livros.h"
 
 void listar_livros() {
-  printf("Ola\n");
+  Livro livro;
+  FILE* arq;
+
+  arq = fopen("livros.dat", "rb");
+
+  if (arq != NULL) {
+    while (fread(&livro, sizeof(livro), 1, arq)) {
+      printf("Codigo: %s", livro.cod);
+      printf("Nome: %s", livro.nome);
+      printf("Autor: %s", livro.autor);
+      printf("Genero: %s", livro.genero);
+      printf("Ano: %d\n", livro.ano);
+      printf("Preco: %f\n", livro.preco);
+      printf("-----------------------\n");
+    }
+    fclose(arq);
+  }
+
 }

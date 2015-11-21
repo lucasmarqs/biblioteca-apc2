998a57e895e4e0215a679876dc35c1077df36966
Lucas Marques <lucas.smarques@outlook.com>
2015-11-21 14:34:21 -0200

    Configurações iniciais

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sistema para Gerencimento de Livros de uma Biblioteca
+
+Trabalho de conclusão de disciplina de APC2, onde temos que trabalhar com manipulação de arquivos em `C`.
+
+**Objetivos**
+  - Cadastrar livros;
+  - Listar todos os livros;
+  - Listar quantidade de livros;
+  - Persistir dados em arquivo.
+
+Alunos:
+  - Lucas Marques (@lucasmarqs)
+  - Mara Regina (@mdomotor)
---
+++ main.c
@@ -0,0 +1,16 @@
+/********************************************************************
+ * Nome Aluno: Lucas da Silva Marques RA: 40795448805
+ * Nome Aluno: Mara Regina Domotor    RA: 42566937850
+ * Data: 30/11/15
+ * Descrição do programa: Cadastrar e listar informações  em arquivo
+ ********************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	printf("Sistema de Gerenciamento da Biblioteca\n\n");
+
+	return 0;
+}
